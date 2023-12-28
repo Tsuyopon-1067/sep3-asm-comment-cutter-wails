6f8650eeafdeee8c0f6d420d4db17507c84d18bc
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2023-12-29 00:38:54 +0900

    :sparkles: feat: comment delete function

--- src/commentDelete.go
+++ src/commentDelete.go
@@ -2,26 +2,34 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"strings"
 )
 
-// App struct
 type SourceCode struct {
 	ctx context.Context
 }
 
-// NewApp creates a new App application struct
 func NewSourceCode() *SourceCode {
 	return &SourceCode{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
 func (a *SourceCode) startup(ctx context.Context) {
 	a.ctx = ctx
 }
 
-// Greet returns a greeting for the given name
-func (a *SourceCode) Delete(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *SourceCode) Delete(input string) string {
+	var result []string
+	lines := strings.Split(input, "\n")
+
+
+	for _, line := range lines {
+		if idx := strings.Index(line, ";"); idx != -1 {
+			line = line[:idx]
+		}
+		if (line != "") {
+			result = append(result, line)
+		}
+	}
+
+	return strings.Join(result, "\n")
 }
